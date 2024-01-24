56a62b81dedf09dc878912a1b78163f547565c8c
Andrew Mackrodt <andrewmackrodt@gmail.com>
2024-01-24 21:46:24 +0000

    add hyperlinks to pagination buttons

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -1,5 +1,8 @@
 export function toSlug(name: string): string {
-  return name.toLowerCase().replaceAll(/[^a-z0-9 -]/gi, '')
+  return name.normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '')
+    .toLowerCase()
+    .replaceAll(/[^a-z0-9 -]/gi, '')
     .replaceAll(/ /g, '-')
     .replaceAll(/-{2,}/g, '-')
     .replace(/-$/, '')
--- src/views/SystemView.vue
+++ src/views/SystemView.vue
@@ -26,48 +26,78 @@ const filters = ref<Record<string, DataTableFilterMetaData>>({
   global: { value: undefined, matchMode: 'contains' },
 })
 
+const ROW_SIZE = 50
+
 const name = ref('')
 const games = ref<GameListItem[] | undefined>()
 const qty = computed(() => games.value ? games.value.reduce((sum, system) => sum + system.qty, 0) : 0)
+const offset = ref(0)
+const page = computed(() => (offset.value / ROW_SIZE) + 1)
 
 async function load(loadData = true) {
   filters.value.global.value = $router.currentRoute.value.query.search
+
+  if (typeof $router.currentRoute.value.query.page === 'string') {
+    const page = parseInt($router.currentRoute.value.query.page)
+    offset.value = (page - 1) * ROW_SIZE
+  }
+
   if (loadData) {
     await Promise.all([
       setRefFromHistory(history, 'name', name, () => getSystemName(props.id)),
       getGames(props.id).then(res => games.value = res?.map(game => {
         const slug = toSlug(game.name)
         return { ...game, slug, route: $router.resolve({
-          name: 'game', params: {
-            systemId: props.id,
-            systemSlug: props.slug,
-            id: game.uid,
-            slug: toSlug(game.name),
-          },
-        }) }
+            name: 'game', params: {
+              systemId: props.id,
+              systemSlug: props.slug,
+              id: game.uid,
+              slug: toSlug(game.name),
+            },
+          }) }
       })),
     ])
   }
+
+  addPaginationLinks()
 }
 
 onBeforeMount(load)
 watch($router.currentRoute, (value, oldValue) => load(value.path !== oldValue.path))
 
-function pushQueryParam(key: string, value: string | undefined) {
-  if (value === $router.currentRoute.value.query[key]) {
-    return
-  }
+function pushQueryParams(params: Record<string, string | undefined>) {
+  let isChanged = false
   const query = Object.assign({}, $route.query)
-  if (value) {
-    query[key] = value
-  } else {
-    delete query[key]
+  for (const [key, value] of Object.entries(params)) {
+    if (value === $router.currentRoute.value.query[key]) {
+      continue
+    }
+    isChanged = true
+    if (value) {
+      query[key] = value
+    } else {
+      delete query[key]
+    }
   }
-  $router.push({ path: $route.fullPath, query })
+  if (isChanged) {
+    return $router.push({ path: $route.fullPath, query })
+  }
 }
 
-function blurSearch() {
-  pushQueryParam('search', filters.value.global.value)
+function updateQueryParams() {
+  pushQueryParams({
+    search: filters.value.global.value,
+    page: page.value.toString(),
+  })
+}
+
+function navigateToGame(game: GameListItemWithRoute) {
+  return $router.push({ ...game.route, state: { systemName: name.value, name: game.name } })
+}
+
+function onClickGame(e: MouseEvent, data: GameListItemWithRoute) {
+  e.preventDefault()
+  return navigateToGame(data)
 }
 
 const paginatorTemplate = [
@@ -80,17 +110,59 @@ const paginatorTemplate = [
   'CurrentPageReport',
 ].join(' ')
 
-function navigateToGame(game: GameListItemWithRoute) {
-  return $router.push({ ...game.route, state: { systemName: name.value, name: game.name } })
+function addPaginationLinks() {
+  const createLink = (el: Element, page: string | number | null, isText = true) => {
+    let a = el.querySelector('a')
+    if (a) {
+      if (page) {
+        a.setAttribute('href', `#${$route.path}?page=${page}`)
+      } else {
+        el.innerHTML = a.innerHTML
+      }
+    } else {
+      if ( ! page) {
+        return
+      }
+      a = document.createElement('a')
+      a.classList.add('page-link')
+      if (isText) {
+        a.classList.add('w-full')
+      }
+      a.setAttribute('href', `#${$route.path}?page=${page}`)
+      a.innerHTML = el.innerHTML
+      el.innerHTML = a.outerHTML
+    }
+  }
+
+  const first = document.querySelector('.p-paginator button.p-paginator-first')
+  const prev = document.querySelector('.p-paginator button.p-paginator-prev')
+  const next = document.querySelector('.p-paginator button.p-paginator-next')
+  const last = document.querySelector('.p-paginator button.p-paginator-last')
+  const maxPage = Math.ceil((games.value?.length ?? 0) / ROW_SIZE)
+
+  if (first) {
+    createLink(first, 1, false)
+  }
+
+  if (prev) {
+    const prevPage = page.value > 1 ? page.value - 1 : null
+    createLink(prev, prevPage, false)
+  }
+
+  if (next) {
+    const nextPage = page.value < maxPage ? page.value + 1 : null
+    createLink(next, nextPage, false)
+  }
+
+  if (last) {
+    createLink(last, maxPage, false)
+  }
+
+  document.querySelectorAll('.p-paginator-pages button').forEach(el => createLink(el, el.textContent!))
 }
 
 function onRowSelect(e: DataTableRowSelectEvent) {
   return navigateToGame(e.data)
-}
-
-function onClickGame(e: MouseEvent, data: GameListItemWithRoute) {
-  e.preventDefault()
-  return navigateToGame(data)
 }
 </script>
 
@@ -103,21 +175,33 @@ function onClickGame(e: MouseEvent, data: GameListItemWithRoute) {
       {{ games ? `${qty.toLocaleString()} codes` : 'Loading' }}
     </p>
     <DataTable
-      v-if="games?.length" v-model:filters="filters" :value="games" data-key="id"
+      v-if="games?.length"
+      v-model:filters="filters"
+      v-model:first="offset"
+      :always-show-paginator="false"
       :global-filter-fields="['name', 'version', 'device.name', 'system.name']"
-      selection-mode="single" size="small" sort-mode="multiple"
-      paginator paginator-position="both" :paginator-template="paginatorTemplate" :page-link-size="8" :rows="50"
+      :page-link-size="8"
+      :paginator-template="paginatorTemplate"
+      :rows="ROW_SIZE"
+      :value="games"
+      data-key="id"
+      paginator
+      paginator-position="both"
+      selection-mode="single"
+      size="small"
+      sort-mode="multiple"
+      @page="updateQueryParams"
       @row-select="onRowSelect"
     >
       <template #header>
         <div class="flex justify-content-end">
           <span class="p-input-icon-left">
             <i class="pi pi-search" />
-            <InputText v-model="filters['global'].value" placeholder="Search" @blur="blurSearch" />
+            <InputText v-model="filters['global'].value" placeholder="Search" @blur="updateQueryParams" @input="() => offset = 0" />
           </span>
         </div>
       </template>
-      <Column header="Game" :sortable="true">
+      <Column field="name" header="Game" :sortable="true">
         <template #body="slotProps">
           <router-link v-slot="{ href }" :to="slotProps.data.route" custom>
             <a class="row-link" :href="href" @click="(e: MouseEvent) => onClickGame(e, slotProps.data)">{{ slotProps.data.name }}</a>
